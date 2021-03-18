390ab38f9173f1d45ea037250d55cdd712761bd5
Parth <pcs003@ucsd.edu>
2021-03-18 11:55:56 -0400

    add reset pley for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+ApplicationRecord.connection.reset_pk_sequence!('users')
+
 u1 = User.create!({email:"demouser@datcord.com", username: "DemoUser", password: "password123", birthdate: "1/1/2000"})
 u2 = User.create!({email:"testuser02@test.com", username: "testuser02", password: "password123", birthdate: "2/2/2000"})
